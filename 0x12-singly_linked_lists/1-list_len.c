8c5aae84e4b54e723f2672de1def6882a6010063
elmah01 <ikpoemmanuel@gmail.com>
2022-10-26 18:21:56 -0700

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of node in a list
- * @h: a given lsit head
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
  *
- * Return: a length of the list
+ * Return: The number of elements in h.
  */
+
 size_t list_len(const list_t *h)
 {
-	size_t length = 0;
+	size_t elements = 0;
 
-	while (h != 0)
+	while (h)
 	{
-		length++;
+		elements++;
 		h = h->next;
 	}
-	return (length);
 
-}
+	return (elements);
+}}
